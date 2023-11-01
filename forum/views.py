fae1d7e383f5f858640deb3761672b6e532353c3
ShaneByrne0963 <>
2023-11-01 15:09:15 +0000

    Add template for new post

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -8,3 +8,13 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by('-posted_on')
     template_name = 'index.html'
     paginate_by = 15
+
+
+class NewPost(View):
+
+    def get(self, request):
+        return render(
+            request,
+            'new_post.html',
+            {},
+        )
