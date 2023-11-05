f99ad9c9db8348c11064f8a42df53c9a65649fc2
ShaneByrne0963 <>
2023-11-05 16:15:16 +0000

    Refactor instance testing into test_instance function

--- forum/core/slug.py
+++ forum/core/slug.py
@@ -1,15 +1,21 @@
+def test_instance(variable, var_type, error_message):
+    """
+    Tests if a variable is of a certain instance, and raises a TypeError
+    if it isn't
+    """
+    if not isinstance(variable, var_type):
+        raise TypeError(error_message)
+
+
 def generate_slug(post_title, post_tag, total_posts):
     """
     Generates a slug for a post using it's title and tag, as well as the total
     number of posts as a seed to generate a set of random characters
     """
     # Error handling
-    if not isinstance(post_title, str):
-        raise TypeError("Title must be a string")
-    if not isinstance(post_tag, str):
-        raise TypeError("Tag must be a string")
-    if not isinstance(total_posts, int):
-        raise TypeError("The total number of posts must be an integer")
+    test_instance(post_title, str, 'title must be a string')
+    test_instance(post_tag, str, 'tag must be a string')
+    test_instance(total_posts, int, 'total_posts must be an integer')
     return ''
 
 
@@ -18,13 +24,15 @@ def generate_character_set(seed, number_of_characters):
     Generates a specified string of characters using a seed
     """
     # Error handling
-    if not isinstance(seed, int):
-        raise TypeError("Seed must be an integer")
-    if not isinstance(number_of_characters, int):
-        raise TypeError("Number of characters must be an integer")
+    test_instance(seed, int, 'seed must be an integer')
+    test_instance(number_of_characters, int,
+                  'num_of_characters must be an integer')
 
     characters = ''
     for i in range(number_of_characters):
         characters += '0'
 
     return characters
+
+
+generate_slug('Title', 'Tag', 42)
--- forum/views.py
+++ forum/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post, Tag
-from .forms import PostForm
 
 
 class PostList(View):
