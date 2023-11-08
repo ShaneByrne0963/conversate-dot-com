0e75c9c545d3403feb1e7712d82d8b37f98d07ed
ShaneByrne0963 <>
2023-11-08 14:30:18 +0000

    Update replies to exist under original comment

--- static/js/comments.js
+++ static/js/comments.js
@@ -2,3 +2,21 @@
 $('.reply-collapse').on('shown.bs.collapse', (event) => {
     event.target.getElementsByClassName('form-control')[0].focus();
 });
+
+
+/**
+ * Is called when the post details page is loaded. Puts replies within their
+ * respective comment
+ */
+function organizeReplies() {
+    let replies = $('.reply');
+    for (let reply of replies) {
+        let commentId = reply.id;
+        commentId = commentId.replace('reply-to-', '');
+        let comment = document.getElementById(`comment-${commentId}`);
+        let replySection = comment.getElementsByClassName('replies')[0];
+        console.log(replySection);
+        replySection.appendChild(reply);
+    }
+}
+window.addEventListener('DOMContentLoaded', organizeReplies);
