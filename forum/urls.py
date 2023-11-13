d4e16279eecb7a4e6b9325afca6847be43d25ff2
ShaneByrne0963 <>
2023-11-13 15:58:56 +0000

    Add button to delete post

--- forum/urls.py
+++ forum/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('new-post', views.AddPost.as_view(), name='new_post'),
     path('edit/<slug:slug>', views.EditPost.as_view(), name='edit_post'),
     path('like/<slug:slug>', views.LikePost.as_view(), name='like_post'),
+    path('delete/<slug:slug>', views.LikePost.as_view(), name='delete_post'),
     path(
         'comment/<slug:slug>',
         views.SendComment.as_view(),
