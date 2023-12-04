372f9e4f83dd826179e5e418e1c57b5499b82cf0
ShaneByrne0963 <>
2023-12-04 19:52:48 +0000

    Update tags to require to be unique

--- static/js/posting.js
+++ static/js/posting.js
@@ -14,5 +14,42 @@ $('#add-tag').click((event) => {
     `;
     $('#tags-list').append(tagElement);
     $('#tag').val('');
-    console.log($('.tag-name').text());
+    setTagAddState(false);
 });
+
+
+// Updates the add tag button to be enabled only when the tag is valid
+$('#tag').on('input', () => {
+    let tagName = $('#tag').val();
+    setTagAddState(tagIsUnique(tagName));
+})
+
+
+/**
+ * Checks if a tag does not already exist in the list of tags
+ * @param {String} tagName the name of the new tag
+ * @returns {Boolean} True if the tag does not already exist
+ */
+function tagIsUnique(tagName) {
+    let existingTags = $('.tag-name').text();
+    let tagList = existingTags.split('#');
+    console.log(tagList);
+    for (let tag of tagList) {
+        if (tagName.toLowerCase() === tag.toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/**
+ * Updates the active state of the "Add Tag" button
+ * @param {Boolean} enabled Whether the button will be set to enabled/disabled
+ */
+function setTagAddState(enabled) {
+    $('#add-tag').removeClass('disabled').removeAttr('disabled');
+    if (!enabled) {
+        $('#add-tag').addClass('disabled').attr('disabled', true);
+    }
+}
