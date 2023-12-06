4a997caa5cd58366de76d60698f5506fca701b8b
ShaneByrne0963 <>
2023-12-06 16:22:37 +0000

    Add tag search instructions below search bar

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,16 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+// Displays instructions on how to search by tag, appearing only once per session
+$('#search-input').focus(() => {
+    if (!sessionStorage.getItem('tag-hint')) {
+        $('#search-hint').removeClass('d-none');
+        sessionStorage.setItem('tag-hint', 'seen');
+    }
+});
+
+// Hiding the search hint when the close button is clicked
+$('#search-hint-close').click(() => {
+    $('#search-hint').addClass('d-none');
+})
