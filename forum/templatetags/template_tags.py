1bb315d70d87d6586631df7224e2a1a397229675
ShaneByrne0963 <>
2023-12-14 11:08:45 +0000

    Refactor modals in poll_list page into 1 poll

--- forum/templatetags/template_tags.py
+++ forum/templatetags/template_tags.py
@@ -13,7 +13,6 @@ def poll_status(poll_id, user_id):
     0: The poll is open and the user has not voted
     >1: The user has voted for the answer number that is returned
     """
-    print('ID: ', poll_id)
     poll = get_object_or_404(Poll, id=int(poll_id))
     answers = list(poll.answers.all())
     for answer in answers:
