543b66f1a42014dea19aca110593f22ab7d0ab4f
ShaneByrne0963 <>
2023-12-14 15:01:44 +0000

    Update logout confirmation to use global modal

--- static/js/modals.js
+++ static/js/modals.js
@@ -1,6 +1,24 @@
+// Deleting posts, comments and polls
 $('.trigger-delete-post').click({ itemType: 'Post' }, setDeleteModal);
 $('.trigger-delete-comment').click({ itemType: 'Comment' }, setDeleteModal);
 $('.trigger-delete-poll').click({ itemType: 'Poll' }, setDeleteModal);
+
+//Logging out
+$('.trigger-logout').click(setLogoutModal);
+
+
+/**
+ * Sets the display information of a modal, and triggers it's display
+ * @param {Object} infoObject format: { title, body, url, textConfirm, ariaCancel }
+ */
+function setModal(infoObject) {
+    $('#modal-all').find('.modal-title').text(infoObject.title);
+    $('#modal-all').find('.modal-body').html(infoObject.body);
+    $('#modal-all').find('.modal-cancel').attr('aria-label', infoObject.ariaCancel);
+    $('#modal-all').find('.modal-confirm').attr('aria-label', infoObject.title)
+        .text(infoObject.textConfirm).attr('href', infoObject.url);
+    $('#modal-all').modal();
+}
 
 
 /**
@@ -9,8 +27,6 @@ $('.trigger-delete-poll').click({ itemType: 'Poll' }, setDeleteModal);
 function setDeleteModal(event) {
     let url = this.getAttribute('data-url');
     let itemType = event.data.itemType;
-    $('#modal-all').find('.modal-title').text(`Conform ${itemType} Delete`);
-
     let bodyHtml = '';
     switch (itemType) {
         case 'Post':
@@ -19,7 +35,7 @@ function setDeleteModal(event) {
             <p>Deleting this post cannot be undone and will remove all likes and comments.</p>`;
             break;
         case 'Comment':
-            bodyHtml =  `<p>Are you sure you want to delete this comment?</p>`;
+            bodyHtml = `<p>Are you sure you want to delete this comment?</p>`;
             let commentParent = this.parentNode.parentNode;
             if (commentParent.querySelector('.reply-toggle')) {
                 bodyHtml += `<p>Deleting this comment will delete all replies to it.</p>`;
@@ -33,8 +49,30 @@ function setDeleteModal(event) {
         default:
             break;
     }
-    $('#modal-all').find('.modal-body').html(bodyHtml);
-    $('#modal-all').find('.modal-cancel').attr('aria-label', `Keep ${itemType}`);
-    $('#modal-all').find('.modal-confirm').attr('aria-label', `Confirm ${itemType} Delete`).text('Delete').attr('href', url);
-    $('#modal-all').modal();
+    let modalObject = {
+        title: `Confirm ${itemType} Delete.`,
+        body: bodyHtml,
+        url: url,
+        textConfirm: 'Delete',
+        ariaCancel: `Keep ${itemType}`
+    };
+    setModal(modalObject);
+}
+
+
+/**
+ * Activates a modal to confirm the logout of a user
+ */
+function setLogoutModal() {
+    let url = this.getAttribute('data-url');
+    let bodyHtml = '<p>Are you sure you want to log out?</p>';
+    
+    let modalObject = {
+        title: `Confirm Logout.`,
+        body: bodyHtml,
+        url: url,
+        textConfirm: 'Log out',
+        ariaCancel: `Stay logged in`
+    };
+    setModal(modalObject);
 }
